197845ca826a5cf76643137219fb0ed7af89bf99
bill-hull <bilhull@gmail.com>
2013-01-17 21:17:24 -0500

    modified native library loading

--- src/main/java/org/java_gtk/gtk/Gtk.java
+++ src/main/java/org/java_gtk/gtk/Gtk.java
@@ -18,6 +18,11 @@
 
 package org.java_gtk.gtk;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * 
  * @author Bill
@@ -28,29 +33,9 @@ public class Gtk {
 	private static native final void gtk_init(java.lang.Object argc, String[] argv);
     private static native final void gtk_main();
     private static native final void gtk_main_quit();
-	
+	    
 	static {
-		System.loadLibrary("libintl-8");
-		System.loadLibrary("zlib1");
-		System.loadLibrary("libexpat-1");
-		System.loadLibrary("libpng14-14");
-		System.loadLibrary("libfreetype-6");
-		System.loadLibrary("libfontconfig-1");
-		System.loadLibrary("libglib-2.0-0");
-		System.loadLibrary("libgmodule-2.0-0");
-		System.loadLibrary("libgthread-2.0-0");
-		System.loadLibrary("libgobject-2.0-0");
-		System.loadLibrary("libpango-1.0-0");
-		System.loadLibrary("libpangowin32-1.0-0");
-		System.loadLibrary("libcairo-2");
-		System.loadLibrary("libatk-1.0-0");
-		System.loadLibrary("libpangoft2-1.0-0");
-		System.loadLibrary("libpangocairo-1.0-0");
-		System.loadLibrary("libgio-2.0-0");
-		System.loadLibrary("libgdk_pixbuf-2.0-0");
-		System.loadLibrary("libgdk-win32-2.0-0");
-		System.loadLibrary("libgtk-win32-2.0-0");
-		System.loadLibrary("libjava-gtk");
+		loadLibraries();
 	}
 
 	private Gtk() {}
@@ -65,6 +50,57 @@ public class Gtk {
     
 	public static void mainQuit() {
 		gtk_main_quit();
+	}
+	
+	private static void loadLibraries() {
+	    Properties props = new Properties();
+		String gtkLibPath = System.getProperty("java.library.path");
+		String javaGtkLibPath = System.getProperty("java.library.path");
+		String libraryExtension = ".dll";
+		
+		InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream("config.properties");
+		if (in != null) {
+			try {
+				props.load(in);
+				String property = props.getProperty("GTKLibraryPath");
+				if (property != null)
+					gtkLibPath = getAbsolutePath(property) + File.separator;
+				property = props.getProperty("JavaGtkLibraryPath");
+				if (property != null)
+					javaGtkLibPath = getAbsolutePath(property) + File.separator;
+				property = props.getProperty("LibraryExtension");
+				if (property != null)
+					libraryExtension = "." + property;
+			} catch (IOException e) {
+
+			}
+		}
+		System.load(gtkLibPath + "libintl-8" + libraryExtension);
+		System.load(gtkLibPath + "zlib1" + libraryExtension);
+		System.load(gtkLibPath + "libexpat-1" + libraryExtension);
+		System.load(gtkLibPath + "libpng14-14" + libraryExtension);
+		System.load(gtkLibPath + "libfreetype-6" + libraryExtension);
+		System.load(gtkLibPath + "libfontconfig-1" + libraryExtension);
+		System.load(gtkLibPath + "libglib-2.0-0" + libraryExtension);
+		System.load(gtkLibPath + "libgmodule-2.0-0" + libraryExtension);
+		System.load(gtkLibPath + "libgthread-2.0-0" + libraryExtension);
+		System.load(gtkLibPath + "libgobject-2.0-0" + libraryExtension);
+		System.load(gtkLibPath + "libpango-1.0-0" + libraryExtension);
+		System.load(gtkLibPath + "libpangowin32-1.0-0" + libraryExtension);
+		System.load(gtkLibPath + "libcairo-2" + libraryExtension);
+		System.load(gtkLibPath + "libatk-1.0-0" + libraryExtension);
+		System.load(gtkLibPath + "libpangoft2-1.0-0" + libraryExtension);
+		System.load(gtkLibPath + "libpangocairo-1.0-0" + libraryExtension);
+		System.load(gtkLibPath + "libgio-2.0-0" + libraryExtension);
+		System.load(gtkLibPath + "libgdk_pixbuf-2.0-0" + libraryExtension);
+		System.load(gtkLibPath + "libgdk-win32-2.0-0" + libraryExtension);
+		System.load(gtkLibPath + "libgtk-win32-2.0-0" + libraryExtension);
+		System.load(javaGtkLibPath + "libjava-gtk" + libraryExtension);
+	}
+	
+	private static String getAbsolutePath(String path) {
+		File f = new File(path);
+		return f.getAbsolutePath();
 	}
 
 }
