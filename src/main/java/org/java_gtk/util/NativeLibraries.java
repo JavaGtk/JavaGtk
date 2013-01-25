2195f0a6460301f1b5bf606067eba7fbe4003674
bill-hull <bilhull@gmail.com>
2013-01-24 23:07:37 -0500

    switched to GTK3 binaries

--- src/main/java/org/java_gtk/util/NativeLibraries.java
+++ src/main/java/org/java_gtk/util/NativeLibraries.java
@@ -9,8 +9,9 @@ public class NativeLibraries {
 	
 	public static void loadLibraries() {
 		Properties props = new Properties();
-		String gtkLibPath = System.getProperty("java.library.path");
-		String javaGtkLibPath = System.getProperty("java.library.path");
+
+		String gtkLibPath = "";
+		String javaGtkLibPath = "";
 		String libraryExtension = ".dll";
 		
 		InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream("config.properties");
@@ -18,39 +19,72 @@ public class NativeLibraries {
 			try {
 				props.load(in);
 				String property = props.getProperty("GTKLibraryPath");
-				if (property != null)
+				if (property != null && !property.isEmpty())
 					gtkLibPath = getAbsolutePath(property) + File.separator;
 				property = props.getProperty("JavaGtkLibraryPath");
-				if (property != null)
+				if (property != null && !property.isEmpty())
 					javaGtkLibPath = getAbsolutePath(property) + File.separator;
 				property = props.getProperty("LibraryExtension");
-				if (property != null)
+				if (property != null && !property.isEmpty())
 					libraryExtension = "." + property;
 			} catch (IOException e) {
 
 			}
 		}
-		System.load(gtkLibPath + "libintl-8" + libraryExtension);
-		System.load(gtkLibPath + "zlib1" + libraryExtension);
-		System.load(gtkLibPath + "libexpat-1" + libraryExtension);
-		System.load(gtkLibPath + "libpng14-14" + libraryExtension);
-		System.load(gtkLibPath + "libfreetype-6" + libraryExtension);
-		System.load(gtkLibPath + "libfontconfig-1" + libraryExtension);
-		System.load(gtkLibPath + "libglib-2.0-0" + libraryExtension);
-		System.load(gtkLibPath + "libgmodule-2.0-0" + libraryExtension);
-		System.load(gtkLibPath + "libgthread-2.0-0" + libraryExtension);
-		System.load(gtkLibPath + "libgobject-2.0-0" + libraryExtension);
-		System.load(gtkLibPath + "libpango-1.0-0" + libraryExtension);
-		System.load(gtkLibPath + "libpangowin32-1.0-0" + libraryExtension);
-		System.load(gtkLibPath + "libcairo-2" + libraryExtension);
-		System.load(gtkLibPath + "libatk-1.0-0" + libraryExtension);
-		System.load(gtkLibPath + "libpangoft2-1.0-0" + libraryExtension);
-		System.load(gtkLibPath + "libpangocairo-1.0-0" + libraryExtension);
-		System.load(gtkLibPath + "libgio-2.0-0" + libraryExtension);
-		System.load(gtkLibPath + "libgdk_pixbuf-2.0-0" + libraryExtension);
-		System.load(gtkLibPath + "libgdk-win32-2.0-0" + libraryExtension);
-		System.load(gtkLibPath + "libgtk-win32-2.0-0" + libraryExtension);
+		if (gtkLibPath == null || gtkLibPath.isEmpty())
+			loadGtkFromSystem();
+		else
+			loadGtkFromConfig(gtkLibPath, libraryExtension);
 		System.load(javaGtkLibPath + "libjava-gtk" + libraryExtension);
+	}
+	
+	private static void loadGtkFromConfig(String gtkLibPath, String libExtension) {
+		System.load(gtkLibPath + "libintl-8" + libExtension);
+		System.load(gtkLibPath + "zlib1" + libExtension);
+		System.load(gtkLibPath + "libffi-6" + libExtension);
+		System.load(gtkLibPath + "libglib-2.0-0" + libExtension);
+		System.load(gtkLibPath + "libgmodule-2.0-0" + libExtension);
+		System.load(gtkLibPath + "libgthread-2.0-0" + libExtension);
+		System.load(gtkLibPath + "libgobject-2.0-0" + libExtension);
+		System.load(gtkLibPath + "libgio-2.0-0" + libExtension);
+		System.load(gtkLibPath + "libatk-1.0-0" + libExtension);
+//		System.load(gtkLibPath + "libexpat-1" + libExtension);
+		System.load(gtkLibPath + "libpng15-15" + libExtension);
+//		System.load(gtkLibPath + "libfreetype-6" + libExtension);
+//		System.load(gtkLibPath + "libfontconfig-1" + libExtension);
+		System.load(gtkLibPath + "libpango-1.0-0" + libExtension);
+		System.load(gtkLibPath + "libpangowin32-1.0-0" + libExtension);
+		System.load(gtkLibPath + "libpixman-1-0" + libExtension);
+		System.load(gtkLibPath + "libcairo-2" + libExtension);
+		System.load(gtkLibPath + "libcairo-gobject-2" + libExtension);
+//		System.load(gtkLibPath + "libpangoft2-1.0-0" + libExtension);
+		System.load(gtkLibPath + "libpangocairo-1.0-0" + libExtension);
+		System.load(gtkLibPath + "libgdk_pixbuf-2.0-0" + libExtension);
+		System.load(gtkLibPath + "libgdk-3-0" + libExtension);
+		System.load(gtkLibPath + "libgtk-3-0" + libExtension);
+	}
+	
+	private static void loadGtkFromSystem() {
+		System.loadLibrary("intl-8");
+		System.loadLibrary("zlib1");
+		System.loadLibrary("expat-1");
+		System.loadLibrary("png14-14");
+		System.loadLibrary("freetype-6");
+		System.loadLibrary("fontconfig-1");
+		System.loadLibrary("glib-2.0-0");
+		System.loadLibrary("gmodule-2.0-0");
+		System.loadLibrary("gthread-2.0-0");
+		System.loadLibrary("gobject-2.0-0");
+		System.loadLibrary("pango-1.0-0");
+		System.loadLibrary("pangowin32-1.0-0");
+		System.loadLibrary("cairo-2");
+		System.loadLibrary("atk-1.0-0");
+		System.loadLibrary("pangoft2-1.0-0");
+		System.loadLibrary("pangocairo-1.0-0");
+		System.loadLibrary("gio-2.0-0");
+		System.loadLibrary("gdk_pixbuf-2.0-0");
+		System.loadLibrary("gdk-win32-2.0-0");
+		System.loadLibrary("gtk-win32-2.0-0");
 	}
 	
 	private static String getAbsolutePath(String path) {
